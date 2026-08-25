3808638a59d7e9c9ecffbf1bc6ba8ff3db2fdee4
윤찬웅 <ssoey2@naver.com>
2026-08-25 13:38:50 +0900

    script 작성 및 불필 file delete

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,30 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref } from 'vue'
+
+// 날씨 데이터
+const weatherList = ref([
+  {id: 'city_01', name: '서울',temp: 28, status: '맑음',humidity: 45,},
+  {id: 'city_02',name: '수원', temp: 24, status: '비', humidity: 72,},
+  {id: 'city_03',name: '부산', temp: 26, status: '구름', humidity: 60,},
+  {id: 'city_04',name: '인천', temp: 30, status: '눈', humidity: 58,},
+])
+
+// 도시 검색
+const searchCity = ref('')
+
+// 상태바
+const Message = ref('카드를 클릭하거나 검색해 보세요.')
+
+// 날씨 카드를 클릭했을 때
+const selectCity = (cityName) => {
+  Message.value = '${cityName}이 선택되었습니다.'
+}
+
+// 상세보기
+const showDetail = (cityName, status, temp, humidaty) => {
+  window.alert(
+    '${cityName}의 현재 날씨는 [${status}] 상태입니다.\n' +
+      '기온은 ${temp}도이고, \n' + 
+      '습도는 ${humidity}%입니다.')
+}
 </script>
-
-<template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
-</template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
