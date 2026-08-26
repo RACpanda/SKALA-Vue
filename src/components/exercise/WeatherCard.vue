6051abdc5dbf2b5adffba158e7e88369119ee806
윤찬웅 <ssoey2@naver.com>
2026-08-26 16:30:17 +0900

    문구 수정

--- src/components/exercise/WeatherCard.vue
+++ src/components/exercise/WeatherCard.vue
@@ -18,22 +18,18 @@ const emit = defineEmits(['click-card'])
         <div >
             <h3>
                 {{ weather.name }}
-                {{ weather.status }}
+                ({{ weather.status }})
             </h3>
-
-            <p>현재 기온 : {{ weather.temp }}도</p>
-            <p>현재 습도 : {{ weather.humidity }}%</p>
-
             <span
                 v-if="weather.temp >= 25"
                 class="temperature-hot">
-                🔥 더움 (25℃ 이상)
+                🔥 더움
             </span>
 
             <span
                 v-else
                 class="temperature-cool">
-                ❄️ 선선함 (25℃ 미만)
+                ❄️ 선선함
             </span>
         </div>
 
