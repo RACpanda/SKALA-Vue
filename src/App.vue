1b9fc2f0cb28b17bca79f43bbcf4e20973ec5689
윤찬웅 <ssoey2@naver.com>
2026-08-26 16:42:06 +0900

    화면 구성 조정

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,19 @@
 <script setup lang="ts"></script>
 
 <template>
-  <nav>
+  <div class = "app-container">
     <h1>과제 4:라우터적용</h1>
 
-    <RouterLink to="/">
-      날씨 대시보드
-    </RouterLink>
+    <nav>
+      <RouterLink to="/">
+        날씨 대시보드
+      </RouterLink>
 
-    <RouterLink to="/about">
-      서비스 소개
-    </RouterLink>
-  </nav>
+      <RouterLink to="/about">
+        서비스 소개
+      </RouterLink>
+    </nav>
 
-  <RouterView />
+    <RouterView />
+  </div>
 </template>
-
-<style scoped>
-.app-container {
-  width: 720px;
-  margin: 40px auto;
-  font-family:
-  Arial, sans-serif;
-}
-
-header h1 {
-  font-size: 32px;
-  margin-bottom: 20px;
-}
-
-nav {
-  display:flex;
-  gap:20px;
-  margin-bottom:30px;
-}
-
-
-nav a {
-  text-decoration:none;
-  color:#555;
-  padding:8px 14px;
-  border-radius:6px;
-}
-
-
-nav a.router-link-active {
-  color:#1976d2;
-  background:#eaf3ff;
-}
-
-
-main {
-  width:100%;
-}
-
-</style>
