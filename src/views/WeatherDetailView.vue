7ddc4123b9797c824651c6a2f72f9ae6e0a598d7
윤찬웅 <ssoey2@naver.com>
2026-08-26 16:10:34 +0900

    Detail 및 About 구현

---
+++ src/views/WeatherDetailView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import {useRoute} from 'vue-router'
+
+const route = useRoute()
+const cityId = route.params.id
+</script>
+
+<template>
+<h2>지역별 상세 기상 관측 정보</h2>
+
+<p>지정지역 :{{cityId}}</p>
+
+</template>
