5677213da2fd9a153a7c631f2c626541d45de3b5
윤찬웅 <ssoey2@naver.com>
2026-08-27 12:50:04 +0900

    api env 및 weatherapi 구현

---
+++ src/api/weatherapi.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+const API_KEY = import.meta.env.VITE_WEATHER_API_KEY
+
+const BASE_URL ='https://api.openweathermap.org/data/2.5/weather'
+
+export const getWeather = async (lat, lon) => {
+    const response = await axios.get(BASE_URL,{
+        params:{
+            lat,
+            lon,
+            appid:API_KEY,
+            units:'metric',
+            lang:'kr'
+        }
+    })
+    return response.data
+}
